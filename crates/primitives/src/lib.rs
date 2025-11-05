dd99c2d66a32a32ffc9d20f19f357b0f39201fcc
David Dada <daveproxy80@gmail.com>
2025-11-05 16:05:47 +0100

    Add primitives lib

--- crates/primitives/src/lib.rs
+++ crates/primitives/src/lib.rs
@@ -1,4 +1,29 @@
+use serde::{Deserialize, Serialize};
+
 pub mod db;
 pub mod error;
 pub mod interfaces;
 pub mod monitor;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Config {
+    pub name: Option<String>,
+    pub indexer: Vec<IndexerConfig>,
+    pub server: ServerConfig,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct ServerConfig {
+    pub db_url: String,
+    pub server_url: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct IndexerConfig {
+    pub event_name: String,
+    pub rpc_url: String,
+    pub address: String,
+    pub event_signature: String,
+    pub block_number: u64,
+    pub db_url: String,
+}
