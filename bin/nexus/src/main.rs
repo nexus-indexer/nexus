0b0b8558f6eb60656014f615c1f177f51dad14ac
KurangaRidwan <kurangaridwan96@gmail.com>
2025-11-07 08:29:24 +0100

    Quick cli

--- bin/nexus/src/main.rs
+++ bin/nexus/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+// This file is part of Nexus.
+
+
+pub mod cli;
+
+#[tokio::main]
+async fn main() {
+    if let Err(err) = cli::run().await {
+        eprintln!("Error: {err:?}");
+        std::process::exit(1);
+    }
 }
